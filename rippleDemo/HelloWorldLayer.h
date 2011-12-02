fd9cba49666deca9f69d82e812dbed4c4cd0ed94
Lars Birkemose <larsbirkemose@lars-birkemoses-macbook-pro.local>
2011-12-02 11:05:53 +0100

    Initial Commit

---
+++ rippleDemo/HelloWorldLayer.h
@@ -0,0 +1,21 @@
+//
+//  HelloWorldLayer.h
+//  rippleDemo
+//
+//  Created by Lars Birkemose on 02/12/11.
+//  Copyright __MyCompanyName__ 2011. All rights reserved.
+//
+
+
+// When you import this file, you import all the cocos2d classes
+#import "cocos2d.h"
+
+// HelloWorldLayer
+@interface HelloWorldLayer : CCLayer
+{
+}
+
+// returns a CCScene that contains the HelloWorldLayer as the only child
++(CCScene *) scene;
+
+@end
---
+++ rippleDemo/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  rippleDemo
+//
+//  Created by Lars Birkemose on 02/12/11.
+//  Copyright __MyCompanyName__ 2011. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+int main(int argc, char *argv[]) {
+    
+    NSAutoreleasePool * pool = [[NSAutoreleasePool alloc] init];
+    int retVal = UIApplicationMain(argc, argv, nil, @"AppDelegate");
+    [pool release];
+    return retVal;
+}
